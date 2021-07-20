1358aa715bf53c4eb8911f3d8f92ebcccfd36b61
Nagireddy Chinta <nagireddy.chinta@gridlogic.in>
2021-07-20 11:33:25 +0530

    push notifications

--- routes/file.js
+++ routes/file.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require("multer");
+var path = require('path')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+    }
+})
+
+var upload = multer({ storage: storage });
+
 router.get('/:file', function (req, res, next) {
-    res.sendFile(req.params.file,  { root: './uploads' })
+    res.sendFile(req.params.file, { root: './uploads' })
 });
+
+router.post('/', upload.single('file'), function (req, res, next) {
+    console.log(req.file)
+    if (req.file) {
+        res.send({ status: "success", file: req.file.filename, errorMessage: "" })
+    }
+    else {
+        res.send({ status: "error", file: "", errorMessage: "Upload has not been successfull" })
+    }
+});
+
 
 module.exports = router;
